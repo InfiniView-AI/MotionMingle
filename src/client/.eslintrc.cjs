226e1f0808af3c110d5189fd36473ac8fda7bc5d
qstommyshu <qstommyshu@gmail.com>
2023-11-12 16:51:22 -0500

    set up: React, TS, Testing-Library, Linter

--- src/client/.eslintrc.cjs
+++ src/client/.eslintrc.cjs
@@ -1,25 +1,25 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
     'airbnb',
     'airbnb-typescript',
     'airbnb/hooks',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  plugins: ['react-refresh'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-      'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react/react-in-jsx-scope': 0,
   },
-}
+};
