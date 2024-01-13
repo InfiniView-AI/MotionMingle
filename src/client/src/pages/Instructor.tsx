a272b57015df3b041f45e98c6dc2bef2dac83e6e
qstommyshu <qstommyshu@gmail.com>
2024-01-12 21:19:42 -0500

    Refactor video elements and add peer connection functionality

--- src/client/src/pages/Instructor.tsx
+++ src/client/src/pages/Instructor.tsx
@@ -1,15 +1,17 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 function Instructor() {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const photoRef = useRef<HTMLCanvasElement>(null);
+  const selfVideoRef = useRef<HTMLVideoElement>(null);
+  const remoteVideoRef = useRef<HTMLVideoElement>(null);
 
   const getVideo = () => {
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: false })
       .then((stream) => {
-        const video = videoRef.current;
+        // add stream to current video element
+        const video = selfVideoRef.current;
         video!.srcObject = stream;
+        // play video from selfVideo Ref
         video?.play();
       })
       .catch((err) => {
@@ -17,27 +19,104 @@ function Instructor() {
       });
   };
 
+  const closeRemote = async (pc: RTCPeerConnection) => {
+    pc.close();
+    const tracks = await remoteVideoRef.current!.srcObject.getTracks().map((track) => track.stop());
+    remoteVideoRef.current!.srcObject = null;
+  };
+
   const closeVideo = () => {
-    const video = videoRef.current;
+    const video = selfVideoRef.current;
     video!.srcObject = null;
   };
+
+  const createPeerConnection = () => {
+    const config = {
+      sdpSemantics: 'unified-plan',
+      iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+    };
+    const pc = new RTCPeerConnection(config);
+    pc.addEventListener('track', (event) => {
+      if (event.track.kind === 'video') {
+        const remoteVideo = remoteVideoRef.current;
+        let rest;
+        [remoteVideo!.srcObject, ...rest] = event.streams;
+        console.log('remoteVideo');
+      }
+    });
+    pc.addTransceiver('video', { direction: 'recvonly' });
+    pc.addTransceiver('audio', { direction: 'recvonly' });
+    return pc;
+  };
+
+  const negotiate = async (pc: RTCPeerConnection) => {
+    // start
+    console.log('negotiate');
+    // load video
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
+    stream.getTracks().forEach((track) => {
+      pc.addTrack(track, stream);
+    });
+
+    // negotiate
+    const offer = await pc?.createOffer();
+    await pc?.setLocalDescription(offer);
+    const requestSdp = pc.localDescription;
+    const sdp = await fetch('http://127.0.0.1:8080/offer', {
+      body: JSON.stringify({
+        sdp: requestSdp?.sdp,
+        type: requestSdp?.type,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+    // console.log(`requestSdp: ${requestSdp?.sdp}`);
+    const answer = await sdp.json();
+    // console.log('answer is :');
+    // console.log(answer);
+    await pc?.setRemoteDescription(answer);
+    remoteVideoRef.current?.play();
+  };
+
+  let pc: RTCPeerConnection;
 
   return (
     <div className="App">
       <div className="camera">
-        <video ref={videoRef}>
+        <video ref={selfVideoRef} width="300" height="200" playsInline>
           <track kind="captions" />
         </video>
       </div>
       <div className="result">
-        <canvas ref={photoRef} />
         <button type="button" onClick={() => getVideo()}>
           Start
         </button>
         <button type="button" onClick={() => closeVideo()}>
           Close
         </button>
+        <button
+          type="button"
+          onClick={() => {
+            pc = createPeerConnection();
+            negotiate(pc);
+          }}
+        >
+          Negotiate
+        </button>
       </div>
+      <div className="remote">
+        <video ref={remoteVideoRef} width="300" height="200" playsInline>
+          <track kind="captions" />
+        </video>
+      </div>
+      <button type="button" onClick={() => closeRemote(pc)}>
+        Close Remote
+      </button>
     </div>
   );
 }
