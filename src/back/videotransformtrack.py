675d8381cd612f9720a00456344f9c293abc3c44
qstommyshu <qstommyshu@gmail.com>
2024-03-17 21:45:09 -0400

    add face in center of mass annotation

--- src/back/videotransformtrack.py
+++ src/back/videotransformtrack.py
@@ -210,13 +210,14 @@ class VideoTransformTrack(MediaStreamTrack):
     
     @staticmethod
     def draw_COM(img, pose_landmarks, t):
-        scale = 200
+        screen_scale = 200
+        pic_scale = 40
         def getCOM(pose_landmarks):
             left_waist = pose_landmarks[23]
             right_waist = pose_landmarks[24]
-            mean_x = (left_waist.x + right_waist.x) / 2
-            mean_y = (left_waist.y + right_waist.y) / 2
-            mean_z = (left_waist.z + right_waist.z) / 2
+            mean_x = (left_waist.x + right_waist.x) / 2 * screen_scale
+            mean_y = (left_waist.y + right_waist.y) / 2 * screen_scale
+            mean_z = (left_waist.z + right_waist.z) / 2 * screen_scale
 
             return (mean_x, mean_y, mean_z)
             
@@ -227,7 +228,33 @@ class VideoTransformTrack(MediaStreamTrack):
             rot_mat = cv2.getRotationMatrix2D(center,angle,1.0)
             new_image = cv2.warpAffine(image, rot_mat, (col,row))
             return new_image
+
+        def get_foot_angle(foot_heel, foot_toe):
+            return math.degrees(math.atan2(foot_heel.z - foot_toe.z, foot_heel.x - foot_toe.x)) + 90
+
         
+        def get_foot_to_com_distance(x, z, com):
+            return (x - com[0]) **2 + (z - com[2]) ** 2
+    
+        def set_footprint_colour(footprint, colour):
+            if colour == "blue":
+                footprint[:, :, 0] = 255
+                footprint[:, :, 1] = 255
+                footprint[:, :, 2] = 0
+            elif colour == "orange":
+                footprint[:, :, 0] = 0
+                footprint[:, :, 1] = 165
+                footprint[:, :, 2] = 255
+            elif colour == "red":
+                footprint[:, :, 1] = 0
+                footprint[:, :, 0] = 0
+                footprint[:, :, 2] = 255
+
+            return footprint
+
+        def get_foot_to_com_distance(x, z, com):
+            return abs((x - com[0]) **2 + (z - com[2]) ** 2)
+
 
 
         left_heel = pose_landmarks[29] # left heel
@@ -236,76 +263,77 @@ class VideoTransformTrack(MediaStreamTrack):
         right_heel = pose_landmarks[30] # right heel
         right_toe = pose_landmarks[32] # right toe
 
-        # angle
-        left_foot_angle = math.degrees(math.atan2(left_heel.z - left_toe.z, left_heel.x - left_toe.x))
-        right_foot_angle = math.degrees(math.atan2(right_heel.z - right_toe.z, right_heel.x - right_toe.x))
+        left_shoulder = pose_landmarks[11]
+        right_shoulder = pose_landmarks[12]
 
+        # angle
+        left_foot_angle = get_foot_angle(left_heel, left_toe)
+        right_foot_angle = get_foot_angle(right_heel, right_toe)
+        shoulder_angle = get_foot_angle(left_shoulder, right_shoulder)
+        # load pics
         right_footprint = cv2.imread("right_footprint.png", -1)
         left_footprint = cv2.imread("left_footprint.png", -1)
+        face = cv2.resize(cv2.imread("face.png", -1), (pic_scale, pic_scale))
+        face = rotateImage(face, shoulder_angle + 90)
         
-
-        right_footprint = cv2.resize(right_footprint, (100, 100))
-        # How does this rotate angle?
+        # resize, change colour and rotate
+        right_footprint = cv2.resize(right_footprint, (pic_scale, pic_scale))
+        right_footprint = set_footprint_colour(right_footprint, "blue")
         right_footprint = rotateImage(right_footprint, right_foot_angle)
 
-        left_footprint = cv2.resize(left_footprint, (100, 100))
+        left_footprint = cv2.resize(left_footprint, (pic_scale, pic_scale))
         left_footprint = rotateImage(left_footprint, left_foot_angle)
+        left_footprint = set_footprint_colour(left_footprint, "orange")
 
-        right_foot_x_offset = math.ceil((right_heel.x + right_toe.x) / 2 * scale)
-        right_foot_y_offset = math.ceil((right_heel.z + right_toe.z) / 2 * scale)
-        right_foot_z_offset = math.ceil((right_heel.z + right_toe.z) / 2 * scale)
+        # calculate offset
+        right_foot_x_offset = math.ceil((right_heel.x + right_toe.x) / 2 * screen_scale)
+        right_foot_y_offset = -math.ceil((right_heel.z + right_toe.z) / 2 * screen_scale) + int(screen_scale / 2)
+        right_foot_z_offset = math.ceil((right_heel.z + right_toe.z) / 2 * screen_scale)
 
-        left_foot_x_offset = math.ceil((left_heel.x + left_toe.x) / 2 * scale) # feet width adjustment
-        left_foot_y_offset = math.ceil((left_heel.z + left_toe.z) / 2 * scale)
-        left_foot_z_offset = math.ceil((left_heel.z + left_toe.z) / 2 * scale)
-
-        lifted_threshold = 0.035
-
-        def get_foot_to_com_distance(x, z, com):
-            return (x - com[0]) **2 + (z - com[2]) ** 2
+        left_foot_x_offset = math.ceil((left_heel.x + left_toe.x) / 2 * screen_scale) # feet width adjustment
+        left_foot_y_offset = -math.ceil((left_heel.z + left_toe.z) / 2 * screen_scale) + int(screen_scale / 2)
+        left_foot_z_offset = math.ceil((left_heel.z + left_toe.z) / 2 * screen_scale)
 
         com = getCOM(pose_landmarks) # correct
+
+        face_x_offset, _, face_z_offset = com
+        face_x_offset = int(face_x_offset)
+        face_y_offset = -int(face_z_offset) + 100
+
+        lifted_threshold = 0.025
+
         l_com_dist = get_foot_to_com_distance(left_foot_x_offset, left_foot_z_offset, com)
         r_com_dist = get_foot_to_com_distance(right_foot_x_offset, right_foot_z_offset, com)
 
         # left heel lifted
-        if left_heel.y - right_heel.y > lifted_threshold:
-            # right_footprint[:, :, 1] = 255
-            left_footprint[:, :, 2] = 255
+        if right_heel.y - left_heel.y > lifted_threshold:
+            right_footprint = set_footprint_colour(right_footprint, "red")
 
         # heel higher, number lower
         # right heel lifted
-        elif right_heel.y - left_heel.y > lifted_threshold:
-            # right_footprint[:, :, 1] = 255
-            right_footprint[:, :, 2] = 255
+        elif left_heel.y - right_heel.y > lifted_threshold:
+            left_footprint = set_footprint_colour(left_footprint, "red")
+
         
         #com calculation have issue due to z axis
-        # # com to right
-        # elif l_com_dist * 0.75 > r_com_dist:
-        #     right_footprint[:, :, 1] = 200
-        #     right_footprint[:, :, 2] = 255
+        # com to right
+        elif l_com_dist * 0.6 > r_com_dist:
+            right_footprint = set_footprint_colour(right_footprint, "red")
+
 
         # # com to left
-        # elif r_com_dist * 0.75 > l_com_dist:
-        #     left_footprint[:, :, 1] = 200
-        #     left_footprint[:, :, 2] = 255
-
-        # print(l_com_dist, r_com_dist)
+        elif r_com_dist * 0.6 > l_com_dist:            
+            left_footprint = set_footprint_colour(left_footprint, "red")
         
-
+        # offset on screen
         right_y1, right_y2 = right_foot_y_offset, right_foot_y_offset + right_footprint.shape[0]
         right_x1, right_x2 = right_foot_x_offset, right_foot_x_offset + right_footprint.shape[1]
 
-        left_y1, left_y2 = left_foot_y_offset, left_foot_y_offset + left_footprint.shape[0]
-        left_x1, left_x2 = left_foot_x_offset, left_foot_x_offset + left_footprint.shape[1]
+        left_y1, left_y2 = left_foot_y_offset , left_foot_y_offset + left_footprint.shape[0]
+        left_x1, left_x2 = left_foot_x_offset , left_foot_x_offset + left_footprint.shape[1]
 
-
-        # right_y1, right_y2 = math.ceil(right_heel.y*scale), math.ceil((right_heel.y * scale) + right_footprint.shape[0])
-        # right_x1, right_x2 = math.ceil(right_heel.x*scale), math.ceil((right_heel.x * scale) + right_footprint.shape[1])
-
-        # left_y1, left_y2 = math.ceil(left_heel.y*scale), math.ceil((left_heel.y * scale) + left_footprint.shape[0])
-        # left_x1, left_x2 = math.ceil(left_heel.x*scale), math.ceil((left_heel.x * scale) + left_footprint.shape[1])
-
+        face_y1, face_y2 = face_y_offset , face_y_offset + face.shape[0]
+        face_x1, face_x2 = face_x_offset , face_x_offset + face.shape[1]
 
 
         if right_footprint.shape[2] == 4:
@@ -317,17 +345,41 @@ class VideoTransformTrack(MediaStreamTrack):
             left_alpha_inv = 1.0 - left_alpha_mask
 
 
+        if right_footprint.shape[2] == 4:
+            # Extract the alpha mask
+            right_alpha_mask = right_footprint[:, :, 3] / 255.0
+            right_alpha_inv = 1.0 - right_alpha_mask
+
+            left_alpha_mask = left_footprint[:, :, 3] / 255.0
+            left_alpha_inv = 1.0 - left_alpha_mask
+
+            face_alpha_mask = face[:, :, 3] / 255.0
+            face_alpha_inv = 1.0 - face_alpha_mask
+
+
+            # TODO: feet position is back side
             # Split the background and overlay in 3 channels
             for c in range(0, 3):
-                if img[right_y1:right_y2, right_x1:right_x2, c].shape[0] != 100 or img[left_y1:left_y2, left_x1:left_x2, c].shape[0] != 100 or img[right_y1:right_y2, right_x1:right_x2, c].shape[1] != 100 or img[left_y1:left_y2, left_x1:left_x2, c].shape[1] != 100:
-                    print("x dimension")
+                # TODO: diff dimension
+                right_bool = img[right_y1:right_y2, right_x1:right_x2, c].shape[0] != pic_scale or img[right_y1:right_y2, right_x1:right_x2, c].shape[1] != pic_scale
+                left_bool = img[left_y1:left_y2, left_x1:left_x2, c].shape[0] != pic_scale or img[left_y1:left_y2, left_x1:left_x2, c].shape[1] != pic_scale
+                face_bool = img[face_y1:face_y2, face_x1:face_x2, c].shape[0] != pic_scale or img[face_y1:face_y2, face_x1:face_x2, c].shape[1] != pic_scale
+                right_out_of_range = right_y2 > screen_scale
+                left_out_of_range = left_y2 > screen_scale
+                if right_bool or left_bool or face_bool or right_out_of_range or left_out_of_range:
                     return
 
+                # print(right_alpha_inv * img[right_y1:right_y2, right_x1:right_x2, c])
+                # right foot
                 img[right_y1:right_y2, right_x1:right_x2, c] = (right_alpha_mask * right_footprint[:, :, c] +
                                           right_alpha_inv * img[right_y1:right_y2, right_x1:right_x2, c])
-                
+                # left foot
                 img[left_y1:left_y2, left_x1:left_x2, c] = (left_alpha_mask * left_footprint[:, :, c] +
                                           left_alpha_inv * img[left_y1:left_y2, left_x1:left_x2, c])
+                
+        
+                img[face_y1:face_y2, face_x1:face_x2, c] = (face_alpha_mask * face[:, :, c] + 
+                                        face_alpha_inv * img[face_y1: face_y2, face_x1: face_x2, c])
 
 
     __TRANSFORMERS: Dict[str, Callable[[VideoFrame], VideoFrame]] = {
